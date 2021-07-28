f11c44c365d1d8230a2eefaf87f2a3e40a5afc14
Vincent Vatelot <vincent.vatelot@gmail.com>
2021-07-28 14:35:23 +0200

    Feature/pydantic (#2)

--- ecoindex/models.py
+++ ecoindex/models.py
@@ -1,9 +1,30 @@
-from dataclasses import dataclass
+from typing import Literal, Optional
+
+from pydantic import BaseModel, Field
 
 
-@dataclass
-class Ecoindex:
-    grade: str
-    score: float
-    ges: float
-    water: float
+class Ecoindex(BaseModel):
+    grade: Optional[Literal["A", "B", "C", "D", "E", "F", "G"]] = Field(
+        default=None,
+        title="Ecoindex grade",
+        description="Is the corresponding ecoindex grade of the page (from A to G)",
+    )
+    score: Optional[float] = Field(
+        default=None,
+        title="Ecoindex score",
+        description="Is the corresponding ecoindex score of the page (0 to 100)",
+        ge=0,
+        le=100,
+    )
+    ges: Optional[float] = Field(
+        default=None,
+        title="Ecoindex GES equivalent",
+        description="Is the equivalent of greenhouse gases emission (in `gCO2e`) of the page",
+        ge=0,
+    )
+    water: Optional[float] = Field(
+        default=None,
+        title="Ecoindex Water equivalent",
+        description="Is the equivalent water consumption (in `cl`) of the page",
+        ge=0,
+    )
---
+++ tests/test_models.py
@@ -0,0 +1,31 @@
+from ecoindex.models import Ecoindex
+from pydantic import ValidationError
+from pytest import raises
+
+
+def test_model_valid():
+    valid_ecoindex = Ecoindex(grade="A", score=99.9, ges=0.6, water=0.1)
+    assert valid_ecoindex.grade == "A"
+    assert valid_ecoindex.score == 99.9
+    assert valid_ecoindex.ges == 0.6
+    assert valid_ecoindex.water == 0.1
+
+
+def test_model_invalid():
+    with raises(ValidationError) as error:
+        Ecoindex(grade="dummy", score="dummy")
+
+    assert (
+        "(type=value_error.const; given=dummy; permitted=('A', 'B', 'C', 'D', 'E', 'F', 'G'))"
+        in str(error.value)
+    )
+
+    assert "value is not a valid float (type=type_error.float)" in str(error.value)
+
+
+def test_ecoindex_model_empty():
+    ecoindex = Ecoindex()
+    assert ecoindex.ges == None
+    assert ecoindex.grade == None
+    assert ecoindex.score == None
+    assert ecoindex.water == None
