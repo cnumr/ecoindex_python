d01b4a8605c116cb0811d8a435f2587d0e582ccf
Vincent Vatelot <vincent.vatelot@gmail.com>
2021-12-16 14:59:44 +0100

    fix(asyncio): enable nest_asyncio in the module

--- ecoindex/__init__.py
+++ ecoindex/__init__.py
@@ -1,3 +1,7 @@
 __version__ = "0.1.0"
+from nest_asyncio import apply as nest_asyncio_apply
+
 from .ecoindex import get_ecoindex
 from .scrap import get_page_analysis
+
+nest_asyncio_apply()
--- test.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-from pprint import pprint
-
-from ecoindex import get_page_analysis
-
-page_analysis = asyncio.run(get_page_analysis(url="http://ecoindex.fr"))
-pprint(page_analysis)
