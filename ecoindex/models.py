c48c69ac3ce3a2ed4edc017cf7f241bc440c0b44
Vincent Vatelot <vincent.vatelot@gmail.com>
2022-04-25 16:39:34 +0200

    chore(split): Remove scrap part from module

--- ecoindex/models.py
+++ ecoindex/models.py
@@ -1,8 +1,5 @@
-from datetime import datetime
-from typing import List, Optional
+from typing import Optional
 
-from pydantic import BaseModel
-from pydantic.networks import HttpUrl
 from sqlmodel import Field, SQLModel
 
 PageType = str
@@ -32,74 +29,4 @@ class Ecoindex(SQLModel):
         title="Ecoindex Water equivalent",
         description="Is the equivalent water consumption (in `cl`) of the page",
         ge=0,
-    )
-
-
-class Page(BaseModel):
-    logs: List
-    outer_html: str
-    nodes: List
-
-
-class PageMetrics(SQLModel):
-    size: float = Field(
-        default=...,
-        title="Page size",
-        description="Is the size of the page and of the downloaded elements of the page in KB",
-        ge=0,
-    )
-    nodes: int = Field(
-        default=...,
-        title="Page nodes",
-        description="Is the number of the DOM elements in the page",
-        ge=0,
-    )
-    requests: int = Field(
-        default=...,
-        title="Page requests",
-        description="Is the number of external requests made by the page",
-        ge=0,
-    )
-
-
-class WindowSize(BaseModel):
-    height: int = Field(
-        default=...,
-        title="Window height",
-        description="Height of the simulated window in pixel",
-    )
-    width: int = Field(
-        default=...,
-        title="Window width",
-        description="Width of the simulated window in pixel",
-    )
-
-    def __str__(self) -> str:
-        return f"{self.width},{self.height}"
-
-
-class WebPage(SQLModel):
-    width: Optional[int] = Field(
-        default=None,
-        title="Page Width",
-        description="Width of the simulated window in pixel",
-    )
-    height: Optional[int] = Field(
-        default=None,
-        title="Page Height",
-        description="Height of the simulated window in pixel",
-    )
-    url: Optional[HttpUrl] = Field(
-        default=None, title="Page url", description="Url of the analysed page"
-    )
-
-
-class Result(Ecoindex, PageMetrics, WebPage):
-    date: Optional[datetime] = Field(
-        default=None, title="Analysis datetime", description="Date of the analysis"
-    )
-    page_type: Optional[PageType] = Field(
-        default=None,
-        title="Page type",
-        description="Is the type of the page, based ton the [opengraph type tag](https://ogp.me/#types)",
     )
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,4 @@
-from ecoindex.models import Ecoindex, Page, Result
+from ecoindex.models import Ecoindex
 from pydantic import ValidationError
 from pytest import raises
 
@@ -29,47 +29,3 @@ def test_ecoindex_model_empty():
     assert ecoindex.grade == None
     assert ecoindex.score == None
     assert ecoindex.water == None
-
-
-def test_model_page():
-    logs = ["Logs of my page"]
-    outer_html = "Html of my page"
-    nodes = ["node1", "node2", "node3"]
-
-    page = Page(
-        logs=logs,
-        outer_html=outer_html,
-        nodes=nodes,
-    )
-
-    assert page.logs == logs
-    assert page.outer_html == outer_html
-    assert page.nodes == nodes
-
-    with raises(ValidationError):
-        Page(logs=logs)
-
-
-def test_result_model():
-    result = Result(
-        size=119,
-        nodes=45,
-        requests=8,
-        url="http://www.myurl.com",
-        width=1920,
-        height=1080,
-        grade="A",
-        score=89,
-        ges=1.22,
-        water=1.89,
-    )
-    assert result.page_type is None
-    assert result.size == 119
-    assert result.nodes == 45
-    assert result.requests == 8
-    assert result.width == 1920
-    assert result.height == 1080
-    assert result.grade == "A"
-    assert result.score == 89
-    assert result.ges == 1.22
-    assert result.water == 1.89
