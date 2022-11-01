e08e02b21d0a91cb4f07147ca1a58e4e6bf78bb1
Vincent Vatelot <vincent.vatelot@gmail.com>
2022-11-01 17:50:07 +0100

    ⬆️ chore(python): Update python version to 3.10

--- ecoindex/models.py
+++ ecoindex/models.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from pkg_resources import get_distribution
 from sqlmodel import Field, SQLModel
 
@@ -7,31 +5,31 @@ PageType = str
 
 
 class Ecoindex(SQLModel):
-    grade: Optional[str] = Field(
+    grade: str | None = Field(
         default=None,
         title="Ecoindex grade",
         description="Is the corresponding ecoindex grade of the page (from A to G)",
     )
-    score: Optional[float] = Field(
+    score: float | None = Field(
         default=None,
         title="Ecoindex score",
         description="Is the corresponding ecoindex score of the page (0 to 100)",
         ge=0,
         le=100,
     )
-    ges: Optional[float] = Field(
+    ges: float | None = Field(
         default=None,
         title="Ecoindex GES equivalent",
         description="Is the equivalent of greenhouse gases emission (in `gCO2e`) of the page",
         ge=0,
     )
-    water: Optional[float] = Field(
+    water: float | None = Field(
         default=None,
         title="Ecoindex Water equivalent",
         description="Is the equivalent water consumption (in `cl`) of the page",
         ge=0,
     )
-    ecoindex_version: Optional[str] = Field(
+    ecoindex_version: str | None = Field(
         default=get_distribution("ecoindex").version,
         title="Ecoindex version",
         description="Is the version of the ecoindex used to compute the score",
