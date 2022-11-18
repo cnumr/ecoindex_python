d952aa872e1bbf56e11d2b9b879fb83b4b010cd8
Vincent Vatelot <vincent.vatelot@ext.adeo.com>
2022-11-18 12:05:55 +0100

    ♻️ refact(sqlmodel): Remove sqlmodel dep and import models from scraper

--- ecoindex/models.py
+++ ecoindex/models.py
@@ -1,10 +1,15 @@
+from datetime import datetime
+from pathlib import Path
+from typing import Any
+
 from pkg_resources import get_distribution
-from sqlmodel import Field, SQLModel
+from pydantic import BaseModel, Field
+from pydantic.networks import HttpUrl
 
 PageType = str
 
 
-class Ecoindex(SQLModel):
+class Ecoindex(BaseModel):
     grade: str | None = Field(
         default=None,
         title="Ecoindex grade",
@@ -34,3 +39,86 @@ class Ecoindex(SQLModel):
         title="Ecoindex version",
         description="Is the version of the ecoindex used to compute the score",
     )
+
+
+class PageMetrics(BaseModel):
+    size: float = Field(
+        default=...,
+        title="Page size",
+        description="Is the size of the page and of the downloaded elements of the page in KB",
+        ge=0,
+    )
+    nodes: int = Field(
+        default=...,
+        title="Page nodes",
+        description="Is the number of the DOM elements in the page",
+        ge=0,
+    )
+    requests: int = Field(
+        default=...,
+        title="Page requests",
+        description="Is the number of external requests made by the page",
+        ge=0,
+    )
+
+
+class WebPage(BaseModel):
+    width: int | None = Field(
+        default=None,
+        title="Page Width",
+        description="Width of the simulated window in pixel",
+    )
+    height: int | None = Field(
+        default=None,
+        title="Page Height",
+        description="Height of the simulated window in pixel",
+    )
+    url: HttpUrl | None = Field(
+        default=None, title="Page url", description="Url of the analysed page"
+    )
+
+
+class WindowSize(BaseModel):
+    height: int = Field(
+        default=...,
+        title="Window height",
+        description="Height of the simulated window in pixel",
+    )
+    width: int = Field(
+        default=...,
+        title="Window width",
+        description="Width of the simulated window in pixel",
+    )
+
+    def __str__(self) -> str:
+        return f"{self.width},{self.height}"
+
+
+class Result(Ecoindex, PageMetrics, WebPage):
+    date: datetime | None = Field(
+        default=None, title="Analysis datetime", description="Date of the analysis"
+    )
+    page_type: PageType | None = Field(
+        default=None,
+        title="Page type",
+        description="Is the type of the page, based ton the [opengraph type tag](https://ogp.me/#types)",
+    )
+
+
+class ScreenShot(BaseModel):
+    id: str
+    folder: str
+
+    def __init__(__pydantic_self__, **data: Any) -> None:
+        super().__init__(**data)
+        path = Path(__pydantic_self__.folder)
+        path.mkdir(parents=True, exist_ok=True)
+
+    def __str__(self) -> str:
+        return f"{self.folder}/{self.id}"
+
+    def get_png(self) -> str:
+        return f"{self.__str__()}.png"
+
+    def get_webp(self) -> str:
+        return f"{self.__str__()}.webp"
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,10 @@
-from ecoindex.models import Ecoindex
+from os import rmdir
+from os.path import isdir
+
 from pydantic import ValidationError
 from pytest import raises
+
+from ecoindex.models import Ecoindex, Result, ScreenShot
 
 
 def test_model_valid():
@@ -30,3 +34,45 @@ def test_ecoindex_model_empty():
     assert ecoindex.grade == None
     assert ecoindex.score == None
     assert ecoindex.water == None
+
+
+def test_result_model():
+    result = Result(
+        size=119,
+        nodes=45,
+        requests=8,
+        url="http://www.myurl.com",
+        width=1920,
+        height=1080,
+        grade="A",
+        score=89,
+        ges=1.22,
+        water=1.89,
+    )
+    assert result.page_type is None
+    assert result.size == 119
+    assert result.nodes == 45
+    assert result.requests == 8
+    assert result.width == 1920
+    assert result.height == 1080
+    assert result.grade == "A"
+    assert result.score == 89
+    assert result.ges == 1.22
+    assert result.water == 1.89
+    assert result.ecoindex_version is not None
+
+
+def test_screenshot_model():
+    id = "screenshot_test_id"
+    folder = "./screenshot_test"
+
+    screenshot = ScreenShot(id=id, folder=folder)
+
+    assert isdir(folder) == True
+    assert screenshot.id == id
+    assert screenshot.folder == folder
+    assert screenshot.get_png() == f"{folder}/{id}.png"
+    assert screenshot.get_webp() == f"{folder}/{id}.webp"
+
+    rmdir(folder)
+    assert isdir(folder) == False
