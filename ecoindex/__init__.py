b09f541dd4663f23d6db418902e7407b0ab7c214
Vincent Vatelot <vincent.vatelot@ik.me>
2023-02-03 15:00:59 +0100

    🎉 feat(values): Get values from reference repository

--- ecoindex/__init__.py
+++ ecoindex/__init__.py
@@ -1,1 +1,5 @@
+from .data.grades import A, B, C, D, E, F, G
+from .data.medians import median_dom, median_req, median_size
+from .data.targets import target_dom, target_req, target_size
 from .ecoindex import get_ecoindex
+from .utils import update_values
