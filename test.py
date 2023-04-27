0cd2f3c99456f0410dd47ba82966802c18f124ae
Vincent Vatelot <vincent.vatelot@ext.adeo.com>
2023-04-27 15:55:50 +0200

    👷 ci(changelog): Remove changelog action

--- test.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from pprint import pprint
-
-from ecoindex import get_ecoindex
-
-# Get ecoindex from DOM elements, size of page and requests of the page
-ecoindex = asyncio.run(get_ecoindex(dom=2240, size=310182.902, requests=254))
-pprint(ecoindex)
